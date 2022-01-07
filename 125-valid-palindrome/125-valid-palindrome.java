5a997a1fa96652e5a4bd749033e2147a02e993c9
avdhesh_gupta <61471356+coderavdhesh@users.noreply.github.com>
2022-01-07 15:33:54 +0530

    Time: 39 ms (15.93%), Space: 43.9 MB (9.19%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -0,0 +1,23 @@
+class Solution {
+     public boolean isPalindrome(String s) {
+         
+        s=replaceAll(s);// replacing all non-alphanumeric characters
+         
+        int i = 0;
+        int j = s.length()-1;
+        
+        while(i<j){
+            if(s.charAt(i) != s.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true; 
+    }
+    
+    public String replaceAll(String s){
+        return s.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
+    }
+    
+    
+}
