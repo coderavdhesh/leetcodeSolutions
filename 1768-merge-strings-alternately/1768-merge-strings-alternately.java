4b7a99091ab2464ff9e854d705388605d98ee55c
avdhesh_gupta <61471356+coderavdhesh@users.noreply.github.com>
2022-01-07 17:44:06 +0530

    Time: 1 ms (55.58%), Space: 39 MB (24.42%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -1,12 +1,6 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
         
-        
-        // NOTE ::::::::
-        // here the Stringbilder is faster than the String method
-        // beCAUSE IT NOT CREATING NEW STRING AGIAN AND AGAIN
-        
-        
         // here the String builder used 
         int i=0, j=0, k=0;
         StringBuffer sb = new StringBuffer();
@@ -23,25 +17,5 @@ class Solution {
         return sb.toString();
         
         
-        // normal extra string use 
-        /*String str = "";
-        int i =0, j=0;
-        int k =0;
-        while(i<word1.length() && j<word2.length())
-        {
-            if(k%2 ==0) 
-                str += word1.charAt(i++);
-            else
-                str += word2.charAt(j++);
-            
-            k++;
-        }
-        while(i<word1.length())
-            str += word1.charAt(i++);
-        while(j<word2.length())
-            str += word2.charAt(j++) ;
-        
-        
-        return str;*/
     }
 }
