dbcf2c8daf494ca68bea1699c598f216070ba59e
avdhesh_gupta <61471356+coderavdhesh@users.noreply.github.com>
2022-01-07 17:39:31 +0530

    Time: 9 ms (9.17%), Space: 40.5 MB (5.08%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        
+        // int i=0, j=0, k=0;
+        // StringBuffer sb = new StringBuffer();
+        // while(i<word1.length() && j<word2.length())
+        //     if(k++%2==0) sb.append(word1.charAt(i++));
+        //     else sb.append(word2.charAt(j++));
+        // while(i<word1.length()) sb.append(word1.charAt(i++));
+        // while(j<word2.length()) sb.append(word2.charAt(j++));
+        // return sb.toString();
+        
+        String str = "";
+        int i =0, j=0;
+        int k =0;
+        while(i<word1.length() && j<word2.length())
+        {
+            if(k%2 ==0) 
+                str += word1.charAt(i++);
+            else
+                str += word2.charAt(j++);
+            
+            k++;
+        }
+        while(i<word1.length())
+            str += word1.charAt(i++);
+        while(j<word2.length())
+            str += word2.charAt(j++) ;
+        
+        
+        return str;
+    }
+}
