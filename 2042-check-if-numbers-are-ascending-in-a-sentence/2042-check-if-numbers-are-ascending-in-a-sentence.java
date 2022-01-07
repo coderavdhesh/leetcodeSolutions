d2e88d1e8b74c309da1ddcb1ee2b3fecfd3b7bfc
avdhesh_gupta <61471356+coderavdhesh@users.noreply.github.com>
2022-01-07 23:55:30 +0530

    Time: 3 ms (45.17%), Space: 38.6 MB (47.75%) - LeetHub

---
+++ 2042-check-if-numbers-are-ascending-in-a-sentence/2042-check-if-numbers-are-ascending-in-a-sentence.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean areNumbersAscending(String s) {
+        
+        /*int prev = -1;
+        
+        for(int i=0; i<s.length() ;i++)
+        {
+            Boolean flag = Character.isDigit(s.charAt(i));
+            if(flag)
+            {
+                int navi = Integer.parseInt(String.valueOf(s.charAt(i)));
+                
+                if(prev>=navi)
+                    return false;
+                
+                prev = Integer.parseInt(String.valueOf(s.charAt(i)));
+            }
+        }
+        return true; */
+        
+        
+        int val = -1;
+        
+        for (String a : s.split(" ")) {
+            if (Character.isDigit(a.charAt(0))) {
+                int n = Integer.parseInt(a);
+                if (val < n) val = n;
+                else return false;
+            }
+        }
+        return true;
+        
+    }
+    
+    
+}
