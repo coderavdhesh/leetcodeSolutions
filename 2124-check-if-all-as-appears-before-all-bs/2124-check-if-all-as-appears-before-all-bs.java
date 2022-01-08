42a61092b9bb3a5fd55ddb5b079cde8bea9134e8
avdhesh_gupta <61471356+coderavdhesh@users.noreply.github.com>
2022-01-08 13:12:30 +0530

    Time: 0 ms (100.00%), Space: 37.2 MB (89.40%) - LeetHub

--- 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.java
+++ 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.java
@@ -1,35 +1,29 @@
 class Solution {
     public boolean checkString(String s) {
     
-//         int a = -1;
-        
-//         for(int i=0; i<s.length()-1; i++)
-//         {
-//           if(s.charAt(i) != s.charAt(i+1) && a>0 || s.charAt(i) == 'a' )
-//                 return false;
-//             else if(s.charAt(i) != s.charAt(i+1) && a<0)
-//             {
-//                 a++;
-//                 continue;
-//             }
-//             else if(s.charAt(i) == s.charAt(i+1) && a<0){
-//                 continue;
-//             }
-            
-//         }
-        
-        int ia=0;
-        for(int i=0; i<s.length(); i++) {
-            if(s.charAt(i) == 'a')
-            {
-                if(ia == -1) 
-                    return false;
-            }
+        int n = 0;
+        for(int i=0; i<s.length(); i++)
+        {
+            if(s.charAt(i) == 'a' )
+            {if(n==-1)
+                    return false;}
             else
-            {
-                 ia = -1;
-            }
-        }    
+                n=-1;
+        }
+        
+        // int ia=0;
+        // for(int i=0; i<s.length(); i++) {
+        //     if(s.charAt(i) == 'a')
+        //     {
+        //         if(ia == -1) 
+        //             return false;
+        //     }
+        //     else
+        //     {
+        //          ia = -1;
+        //     }
+        // } 
+        
         return true ;
             
     }
