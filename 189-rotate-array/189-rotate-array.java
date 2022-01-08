ab4cde1f2d232440a4336fb102b3cb88f8d0b073
avdhesh_gupta <61471356+coderavdhesh@users.noreply.github.com>
2022-01-08 22:56:35 +0530

    Time: 1 ms (68.85%), Space: 59.1 MB (29.90%) - LeetHub

---
+++ 189-rotate-array/189-rotate-array.java
@@ -0,0 +1,57 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        
+      // while(k>0){      // O(N^2) ... TLE Error
+      //   int temp = nums[nums.length-1];
+      //   for(int i=nums.length-1; i>0; i--)
+      //   {
+      //       nums[i]=nums[i-1];
+      //   }
+      //   nums[0] = temp;
+      //   k--;
+      //   }
+        
+        
+//         if(nums.length <2)
+//             return;
+        
+//         int arr[] = new int[k];
+//         int j=0;
+//         for(int i=nums.length-k; i<nums.length; i++)
+//         {
+//             arr[j] = nums[i];j++;
+//         }
+        
+//         int n = nums.length-1;
+//         for(int i=nums.length-1-k; i>=0;i--)
+//         {
+//             nums[n] = nums[i];n--;
+//         }
+//         for(int i=0; i<k; i++)
+//         {
+//             nums[i] = arr[i];
+//         }
+        
+        
+        // best optimized solution o(n/2)
+        
+        k = k% nums.length; // agar k>arraylength, yen it wil take the modulo of k, coz that much reverse is required 
+        reverse(nums,0,nums.length-1-k);
+        reverse(nums, nums.length-k, nums.length-1);
+        reverse(nums, 0, nums.length-1);
+        
+    }
+    
+    public int[] reverse(int[] arr, int left, int right)
+    {
+        if(left>right)
+            return arr;
+        
+        int temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+        
+        return reverse(arr,left+1, right-1);// recursion
+    }
+    
+}
