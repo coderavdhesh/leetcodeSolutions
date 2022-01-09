da026fb2a9ea89de42eff0044bb4a60b9b7c66eb
avdhesh_gupta <61471356+coderavdhesh@users.noreply.github.com>
2022-01-09 11:24:16 +0530

    Time: 7 ms (43.52%), Space: 42 MB (19.65%) - LeetHub

--- 345-reverse-vowels-of-a-string/345-reverse-vowels-of-a-string.java
+++ 345-reverse-vowels-of-a-string/345-reverse-vowels-of-a-string.java
@@ -1,6 +1,6 @@
 class Solution {
-    public String reverseVowels(String s) {
-        
+    /*public String reverseVowels(String s) {
+        // Method one : using set to store vowels
         char arr[]=s.toCharArray();
         Set<Character>vowel=Set.of('a','e','i','o','u','A','E','I','O','U');// important--- set of the vowels 
         int l=0,r=s.length()-1;
@@ -19,6 +19,41 @@ class Solution {
           else 
               r--;
         }
-        return String.valueOf(arr);
+        return String.valueOf(arr);  .
+    }*/
+    
+    public static boolean IsVowel(char ch){
+        String vowels = "aeiouAEIOU";
+        int idx = vowels.indexOf(ch);
+        
+        if(idx == -1){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+    
+    // method 2 : creat a str of vowels and checking the char belongs to it or not---
+    public String reverseVowels(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+        
+        char[] arr = s.toCharArray();
+        
+        while(i < j){
+            while(i < j && IsVowel(arr[i]) == false){
+                i++;
+            }
+            while(i < j && IsVowel(arr[j]) == false){
+                j--;
+            }
+            char temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+        return new String(arr);
     }
 }
