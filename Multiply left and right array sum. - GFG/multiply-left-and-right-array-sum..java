3522305f93c86b4f6d8e85677d10a9a29ab99a34
avdhesh_gupta <61471356+coderavdhesh@users.noreply.github.com>
2022-02-01 22:43:25 +0530

    Added solution - LeetHub

---
+++ Multiply left and right array sum. - GFG/multiply-left-and-right-array-sum..java
@@ -0,0 +1,74 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+//Initial Template for Java
+
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+
+class Array {
+    
+    // Driver code
+	public static void main (String[] args) throws IOException{
+		// Taking input using buffered reader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		// looping through all testcases
+		while(testcases-- > 0){
+		    String line = br.readLine();
+		    String[] element = line.trim().split("\\s+");
+		    int N = Integer.parseInt(element[0]);
+		    int arr [] = new int[N];
+		    line = br.readLine();
+		    String[] elements = line.trim().split("\\s+");
+		    for(int i = 0;i<N;i++){
+	            arr[i] = Integer.parseInt(elements[i]);    
+	        }
+		    
+		    Complete obj = new Complete();
+		    int ans = obj.multiply(arr, N);
+        	System.out.println(ans);
+		}
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Complete{
+    
+   
+    // Function for finding maximum and value pair
+    public static int multiply (int arr[], int n) {
+        //Complete the function
+        int leftsum = 0;
+        int rightsum = 0;
+        
+        for(int i=0; i<n/2; i++)
+        {
+            leftsum += arr[i];
+        }
+        for(int i=n/2; i<n; i++)
+        {
+            rightsum += arr[i];
+        }
+        
+        return leftsum*rightsum;
+    }
+    
+    
+}
+
+
+
+
+
