4db65fb4b4241c573fde0f4cff0a0d5aefa4a717
avdhesh_gupta <61471356+coderavdhesh@users.noreply.github.com>
2022-02-05 23:08:36 +0530

    Added solution - LeetHub

--- Bitonic Point - GFG/bitonic-point.java
+++ Bitonic Point - GFG/bitonic-point.java
@@ -42,14 +42,13 @@ class Solution {
     while(s<=e)
     {
         int mid=s+(e-s)/2;
-        
         if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1])
-            return arr[mid];
+        return arr[mid];
         else if(arr[mid]<arr[mid-1] && arr[mid]>arr[mid+1])
-            e=mid-1;
+        {
+        e=mid-1;
+        }
         else
-            s=mid+1;
-    }
-    return -1;
-    }
+        s=mid+1;
+
 }
