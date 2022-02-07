05b8a165d6cc6104efbe7f8511cd79c90d8d1afe
avdhesh_gupta <61471356+coderavdhesh@users.noreply.github.com>
2022-02-07 20:30:46 +0530

    Added solution - LeetHub

---
+++ A guy with a mental problem - GFG/a-guy-with-a-mental-problem.java
@@ -0,0 +1,69 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while(t-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            long a[] = new long[(int)(n)];
+            long b[] = new long[(int)(n)];
+            
+            
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Long.parseLong(inputLine[i]);
+            }
+            String inputLine1[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                b[i] = Long.parseLong(inputLine1[i]);
+            }
+            
+            
+            Solution obj = new Solution();
+            System.out.println(obj.minTime(a, b, n));
+        }
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+
+    public long minTime(long a[], long b[], long n) {
+        // Your code goes here 
+        
+        long time_A = 0;
+        long time_B = 0;
+        
+        int i =0;
+        while(i<n)
+        {
+            if(i%2 ==0)
+            {
+                time_A += a[i];
+                time_B += b[i];
+            }
+            else
+            {
+                time_A += b[i];
+                time_B += a[i];
+            }
+            i++;
+            
+        }
+        
+        return time_A < time_B ? time_A : time_B ;
+        
+    }
+}
