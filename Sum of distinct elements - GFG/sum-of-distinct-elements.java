e805c902b7e78d3ffa8e9c44972332b580ebe806
avdhesh_gupta <61471356+coderavdhesh@users.noreply.github.com>
2022-02-16 00:31:11 +0530

    Added solution - LeetHub

---
+++ Sum of distinct elements - GFG/sum-of-distinct-elements.java
@@ -0,0 +1,53 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+
+
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            int ans = new Solution().findSum(arr, n);
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution {
+    int findSum(int arr[], int n) {
+        // code here
+        
+        HashSet<Integer> set = new HashSet<>();
+        
+        for(int elements : arr)
+        {
+            set.add(elements);
+        }
+        int sum = 0;
+        for(int nums : set)
+        {
+            sum += nums;
+        }
+        return sum;
+    }
+}
