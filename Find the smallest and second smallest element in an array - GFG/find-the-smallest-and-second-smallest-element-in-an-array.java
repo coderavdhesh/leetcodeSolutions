f94190acf7d46e0a67c83f54158646b62ced5ec2
avdhesh_gupta <61471356+coderavdhesh@users.noreply.github.com>
2022-05-10 22:34:03 +0530

    Added solution - LeetHub

---
+++ Find the smallest and second smallest element in an array - GFG/find-the-smallest-and-second-smallest-element-in-an-array.java
@@ -0,0 +1,77 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            long n = Long.parseLong(br.readLine().trim());
+            long a[] = new long[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Long.parseLong(inputLine[i]);
+            }
+            
+            Compute obj = new Compute();
+            long[] product = obj.minAnd2ndMin(a, n); 
+            if(product[0]==-1)
+                System.out.println(product[0]);
+            else
+                System.out.println(product[0]+" "+product[1]);
+            
+        }
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+//PriorityQueue<Integer> pQueue = new PriorityQueue<Integer>(Collections.reverseOrder());
+
+class Compute 
+{
+    public long[] minAnd2ndMin(long a[], long n)  
+    {
+        
+        if( a.length < 2)
+          return new long[]{-1};
+        
+        PriorityQueue<Long> maxHeap = new PriorityQueue<Long>(Collections.reverseOrder());
+        
+        for(long element : a)
+        {
+            if(!maxHeap.contains(element))
+                 maxHeap.offer(element);
+            else
+                continue;
+            
+            if(maxHeap.size() > 2)
+                 maxHeap.poll();
+        }
+        
+        if(maxHeap.size() < 2)
+           return new long[]{-1};
+           
+        long[] arr = new long[2];
+        
+        for(int i =1; i>=0; i--)
+        {
+            arr[i] = maxHeap.poll();
+        }
+        
+        return arr;
+        
+    }
+}
