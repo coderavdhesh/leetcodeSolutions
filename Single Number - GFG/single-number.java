66122972434b30edf05ff9af8653b31eb6fb5a07
avdhesh_gupta <61471356+coderavdhesh@users.noreply.github.com>
2022-05-11 09:41:25 +0530

    Added solution - LeetHub

---
+++ Single Number - GFG/single-number.java
@@ -0,0 +1,48 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            int ans = new Solution().getSingle(arr, n);
+            System.out.println(ans);
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+class Solution {
+    int getSingle(int arr[], int n) {
+        // code here
+        
+        // we can use BitManupilation XOR
+        
+        int Xor = 0;
+        
+        for( int element :  arr)
+        {
+            Xor ^= element;
+        }
+        
+        return Xor;
+    }
+}
