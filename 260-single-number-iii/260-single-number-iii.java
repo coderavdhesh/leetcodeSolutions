31ddd1161c290f37fce61f8de0194b97767b7ea9
avdhesh_gupta <61471356+coderavdhesh@users.noreply.github.com>
2022-05-11 17:18:03 +0530

    Time: 8 ms (15.80%), Space: 47.1 MB (5.53%) - LeetHub

---
+++ 260-single-number-iii/260-single-number-iii.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for(int i : nums)
+        {
+            map.put(i, map.get(i)==null ? 1 : map.get(i)+1 );
+        }
+        
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for(int i : nums)
+        {
+            if(map.get(i) == 1)
+                list.add(i);
+        }
+        
+        int[] arr = new int[list.size()];
+        
+        for(int i =0; i <list.size(); i++)
+        {
+            arr[i] = list.get(i);
+        }
+        
+        return arr;
+        
+    }
+}
