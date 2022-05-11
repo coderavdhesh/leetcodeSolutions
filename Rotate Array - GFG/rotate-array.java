2930bb541f295b0a196bb2a724c7129dea513c3f
avdhesh_gupta <61471356+coderavdhesh@users.noreply.github.com>
2022-05-11 18:56:30 +0530

    Added solution - LeetHub

--- Rotate Array - GFG/rotate-array.java
+++ Rotate Array - GFG/rotate-array.java
@@ -1,29 +1,79 @@
-/*package whatever //do not write package name here */
+// { Driver Code Starts
+//Initial Template for Java
 
 import java.util.*;
-import java.lang.*;
 import java.io.*;
+import java.lang.*;
 
-class GFG {
-	public static void main (String[] args) {
-	    Scanner sc = new Scanner(System.in);
- int t = sc.nextInt();
- 
- while(t--> 0){
-     
-     int n = sc.nextInt();
-     int d = sc.nextInt();
-     
-     int a[] = new int[n];
-     for(int i = 0 ; i< n ; i++){
-         a[i] = sc.nextInt(); 
-     }
-     
-    for(int i = 0 ; i< n; i++){
-        
-        System.out.print(a[(d+i)%n] +" ");
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+        PrintWriter out=new PrintWriter(System.out);
+        //taking testcases
+        int t = Integer.parseInt(br.readLine()); 
+    	for(int i=0;i<t;i++)
+    	{
+            String str=br.readLine();
+    		String[] starr=str.split(" ");
+    		
+    		//input n and d
+    	    int n=Integer.parseInt(starr[0]);
+    		int d= Integer.parseInt(starr[1]);
+    		
+            int[] arr=new int[n];
+    		String str1=br.readLine();
+    		String[] starr1=str1.split(" ");
+    		
+    		//inserting elements in the array
+    		for(int j=0;j<n;j++)
+    		{
+    		  arr[j]= Integer.parseInt(starr1[j]);
+    		}
+    		
+    		//calling rotateArr() function
+            new Solution().rotateArr(arr, d, n);
+    		StringBuffer sb=new StringBuffer();
+    		
+    		//printing the elements of the array
+            for(int t1=0;t1<n;t1++)
+                sb.append(arr[t1]+" ");
+            out.println(sb);
+         }
+         out.close();
     }
-    System.out.println();
 }
-	}
-	}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to rotate an array by d elements in counter-clockwise direction. 
+    static void rotateArr(int arr[], int d, int n)
+    {
+        // add your code here
+        
+        int modLength = d%n;
+        
+        flip(arr, 0, modLength-1);
+        flip(arr, modLength, n-1);
+        
+        flip(arr, 0, n-1);
+        
+    }
+    static void flip(int[] ar , int start, int end)
+    {
+        while(start<= end)
+        {
+            int temp = ar[start];
+            ar[start] = ar[end];
+            ar[end] = temp;
+            
+            start++;
+            end--;
+        }
+    }
+}
