1af129cabb852a9177e6a66fa43374d39992012f
avdhesh_gupta <61471356+coderavdhesh@users.noreply.github.com>
2022-05-28 18:16:00 +0530

    Time: 2 ms (61.39%), Space: 54.4 MB (47.19%) - LeetHub

--- 961-n-repeated-element-in-size-2n-array/961-n-repeated-element-in-size-2n-array.java
+++ 961-n-repeated-element-in-size-2n-array/961-n-repeated-element-in-size-2n-array.java
@@ -1,21 +1,33 @@
 class Solution {
     public int repeatedNTimes(int[] nums) {
         
-        HashMap<Integer, Integer> map = new HashMap<>();
+//         HashMap<Integer, Integer> map = new HashMap<>();
+        
+//         for(int i : nums)
+//         {
+//             map.put(i, map.get(i) == null ? 1 : map.get(i)+1);
+//         }
+    
+//         // this line is imp.... i have learned it here for the first time
+//         for(int i : map.keySet())
+//         {
+//             if(map.get(i) == nums.length/2){
+//                 return i;
+//             }
+//         }
+//         return 0;
+        
+        
+        HashSet<Integer> set = new HashSet<>();
         
         for(int i : nums)
         {
-            map.put(i, map.get(i) == null ? 1 : map.get(i)+1);
-        }
-    
-        // this line is imp.... i have learned it here for the first time
-        for(int i : map.keySet())
-        {
-            if(map.get(i) == nums.length/2){
+            if(!set.contains(i))
+                set.add(i);
+            else
                 return i;
-            }
         }
-        return 0;
         
+        return 0;
     }
 }
