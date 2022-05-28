e1bb19f8da35ddb1f34669453e4fa0a9007dcf9f
avdhesh_gupta <61471356+coderavdhesh@users.noreply.github.com>
2022-05-28 14:02:07 +0530

    Time: 17 ms (6.58%), Space: 54.5 MB (17.14%) - LeetHub

--- 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.java
+++ 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.java
@@ -1,9 +1,32 @@
 class Solution {
     public int maxProductDifference(int[] nums) {
         
-        Arrays.sort(nums);
+        // this code is having nlogn time 
+//         Arrays.sort(nums);
         
-        return (nums[nums.length-1]*nums[nums.length-2]) - (nums[1]*nums[0]);
+//         return (nums[nums.length-1]*nums[nums.length-2]) - (nums[1]*nums[0]);
         
+        
+        // use the min and max heap to get the O(N) time
+        
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        
+        
+        for(int i : nums)
+        {
+            minHeap.offer(i);
+            
+            if(minHeap.size()>2)
+                minHeap.poll();
+            
+            maxHeap.offer(i);
+            
+            if(maxHeap.size()>2)
+                maxHeap.poll();
+            
+        }
+        
+        return (minHeap.poll() * minHeap.peek()) - (maxHeap.poll() * maxHeap.peek());
     }
 }
