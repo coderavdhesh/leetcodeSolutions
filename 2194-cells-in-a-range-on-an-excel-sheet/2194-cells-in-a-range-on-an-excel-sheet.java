9bb42b14711618e5db9f94afd7af8ea7ea729665
avdhesh_gupta <61471356+coderavdhesh@users.noreply.github.com>
2022-05-29 09:22:26 +0530

    Time: 2 ms (96.46%), Space: 47.8 MB (28.51%) - LeetHub

--- 2194-cells-in-a-range-on-an-excel-sheet/2194-cells-in-a-range-on-an-excel-sheet.java
+++ 2194-cells-in-a-range-on-an-excel-sheet/2194-cells-in-a-range-on-an-excel-sheet.java
@@ -14,54 +14,13 @@ class Solution {
             for(char j =a; j<=b; j++)
             {
                 // this line is imp
-                // strList.add(new String(new char[]{i, j}));
-                String str = Character.toString(i) + Character.toString(j);
-                strList.add(str);
+                strList.add(new String(new char[]{i, j}));
+                // String str = Character.toString(i) + Character.toString(j);
+                // strList.add(str);
             }
         }
         
         return strList;
         
-        
-//         List<String> l = new ArrayList<>();
-//         int c1 = (int)s.charAt(0);
-//     int c2 = (int)s.charAt(3);
-//     int r1 = (int)s.charAt(1);
-//     int r2 = (int)s.charAt(4);
-	
-//     for(int i=c1; i<=c2; i++){
-//         char ch = (char)i;
-//         for(int j=r1; j<=r2; j++){
-//             char n = (char)j;
-//             String s = Character.toString(ch) + Character.toString(n);
-//             l.add(s);
-//         }
-//     }
-//     return l;
-        
-        
-        
-//         char [] c=s.toCharArray();
-//         List str= new ArrayList<>();
-    
-//         for(char i=c[0];i<=c[3];i++)
-//         {
-//             for(char j=c[1];j<=c[4];j++)
-//                 str.add(new String(new char[]{i, j}));
-//         }
-//             return str;  
-        
-        
-//     // char sc = s.charAt(0), ec = s.charAt(3);
-//     // char sr = s.charAt(1), er = s.charAt(4);
-//     // List res = new ArrayList<>();
-    
-//     // for (char i = sc; i <= ec; ++i){
-//     // for (char j = sr; j <= er; ++j){
-//     // res.add(new String(new char[]{i, j}));
-//     // }
-//     // }
-    
-//     // return res;
     }
 }
