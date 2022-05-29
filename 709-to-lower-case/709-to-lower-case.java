f9b9ecb16ce9e1a45ba8cc4776954cc34fa678f0
avdhesh_gupta <61471356+coderavdhesh@users.noreply.github.com>
2022-05-29 09:49:02 +0530

    Time: 1 ms (62.99%), Space: 42.3 MB (41.10%) - LeetHub

--- 709-to-lower-case/709-to-lower-case.java
+++ 709-to-lower-case/709-to-lower-case.java
@@ -1,7 +1,17 @@
 class Solution {
     public String toLowerCase(String s) {
         
-        return s.toLowerCase();
+        //return s.toLowerCase();
+        
+        char[] ch = s.toCharArray();
+        
+        for(int i =0; i<ch.length; i++ )
+        {
+            if(ch[i]>64 && ch[i]<91)
+                ch[i] += 32;
+        }
+        
+        return String.valueOf(ch);
         
     }
 }
