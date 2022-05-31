f2ed151c941df82f0048f6daec2c3def6786d808
avdhesh_gupta <61471356+coderavdhesh@users.noreply.github.com>
2022-05-31 16:06:42 +0530

    Time: 4 ms (8.28%), Space: 41.6 MB (65.45%) - LeetHub

--- 747-largest-number-at-least-twice-of-others/747-largest-number-at-least-twice-of-others.java
+++ 747-largest-number-at-least-twice-of-others/747-largest-number-at-least-twice-of-others.java
@@ -1,6 +1,8 @@
 class Solution {
     public int dominantIndex(int[] nums) {
         
+        // this code is having O(n) but space is not satisfactory
+    
         if(nums.length < 2)
             return 0;
         
@@ -26,6 +28,32 @@ class Solution {
         }
         
         return (max >= temp+temp) ? index : -1;
+        
+        
+        // this brute force will not work for few cases....
+        /* 
+        if(nums.length < 2)
+            return 0;
+        
+        int max1 = nums[0];
+        int max2 = Integer.MIN_VALUE;
+        int index = -1;
+        
+        for(int i =0; i<nums.length; i++)
+        {
+            if(nums[i] > max1)
+            {
+                int temp = max1;
+                max1 = nums[i];
+                max2 = temp;
+                
+                index = i;
+            }
+            if(nums[i] > max2 && nums[i]< max1)
+                max2 = nums[i];
+        }
+        
+        return (max1 >= max2 + max2) ? index : -1; */
     
     }
 }
