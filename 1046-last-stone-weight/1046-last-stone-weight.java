8dd925425b59aa44c1a4699765ace79869b11d90
avdhesh_gupta <61471356+coderavdhesh@users.noreply.github.com>
2022-06-01 00:28:51 +0530

    Time: 3 ms (32.72%), Space: 40.9 MB (76.28%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.java
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -1,6 +1,7 @@
 class Solution {
     public int lastStoneWeight(int[] stones) {
         
+        /*
         if(stones.length < 2)
             return stones[0];
         
@@ -17,11 +18,24 @@ class Solution {
             weight = Math.abs(y-weight);
             
             maxHeap.offer(weight);
-            weight = maxHeap.poll();
+            weight = maxHeap.poll();// yaha dimag lagana pada ache se
         }
         
         
-        return weight;
+        return weight;*/
+        
+        //
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for(int stone : stones) {
+            pq.offer(stone);
+        }
+        
+        while(pq.size() > 1) {
+            pq.offer(pq.poll() - pq.poll()); 
+        }
+        
+        return pq.poll();
         
     }
 }
