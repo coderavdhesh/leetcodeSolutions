d7802cbc007fc6228857f84c5c199f1506a09df9
avdhesh_gupta <61471356+coderavdhesh@users.noreply.github.com>
2022-05-31 23:05:30 +0530

    Time: 53 ms (33.96%), Space: 54.4 MB (44.75%) - LeetHub

--- 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.java
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.java
@@ -1,7 +1,10 @@
 class Solution {
     public int countCharacters(String[] words, String chars) {
         
-        int length = 0;
+        // ye code swayam mene apne haatho se likha hain ... full imaandari
+        // # love_for_code++ , but this code is having O(n^2) time
+        
+        /*int length = 0;
         
         HashMap<Character, Integer> map = new HashMap<>();
         
@@ -31,6 +34,31 @@ class Solution {
                 length += str.length();
         }
         
-        return length;
+        return length;*/
+        
+         Map<Character, Integer> map = new HashMap<>();
+        for(char c: chars.toCharArray())
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        
+        int ans = 0;
+        for(String s: words) {
+            Map<Character, Integer> count = new HashMap<>();
+            int i = 0;
+            for(; i<s.length(); i++) {
+                char c = s.charAt(i);
+                if(map.containsKey(c)) {
+                    if(count.containsKey(c) && count.get(c) + 1 > map.get(c))
+                        break;
+                    count.put(c, count.getOrDefault(c, 0) + 1);
+                } else
+                    break;
+            }
+            if(i == s.length())
+                ans += s.length();
+        }
+        
+        return ans;
+        
+        
     }
 }
