5066e5983f73e9533cfbbc53af2711f426eda01b
avdhesh_gupta <61471356+coderavdhesh@users.noreply.github.com>
2022-05-31 13:34:20 +0530

    Time: 9 ms (6.73%), Space: 45.7 MB (17.16%) - LeetHub

--- 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.java
+++ 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.java
@@ -1,6 +1,7 @@
 class Solution {
     public int findFinalValue(int[] nums, int original) {
         
+        // with the help of hashset with O(n) time -- best solution
         HashSet<Integer> set = new HashSet<>();
         
         for(int i : nums) set.add(i);
@@ -11,6 +12,12 @@ class Solution {
                 original *= 2;
         }
         
+        // we can also sort the array first and the apply the binary search to get the element, this code will give me O(nlogn) time compexity
+        
+        // we can also do this with brute force approch of the double loop and it will give me the compexity of O(n*n), joo ki bekar solution hain
+        
         return original;
+        
+        
     }
 }
