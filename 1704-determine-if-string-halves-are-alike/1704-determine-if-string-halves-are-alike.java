b5f34ded8509cdf90c6ddc9db5f184f13367dbed
avdhesh_gupta <61471356+coderavdhesh@users.noreply.github.com>
2022-06-02 19:18:40 +0530

    Time: 5 ms (57.06%), Space: 41.8 MB (71.10%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
@@ -1,22 +1,44 @@
 class Solution {
     public boolean halvesAreAlike(String s) {
         
-        ArrayList<Character> list = new ArrayList<Character>();
-        list.add('a');list.add('e'); list.add('i');list.add('o'); list.add('u'); list.add('A'); list.add('E'); list.add('I'); list.add('O'); list.add('U');
+//         ArrayList<Character> list = new ArrayList<Character>();
+//         list.add('a');list.add('e'); list.add('i');list.add('o'); list.add('u'); list.add('A'); list.add('E'); list.add('I'); list.add('O'); list.add('U');
+        
+//         int count1 =0;
+//         int count2 =0;
+        
+//         for(int i =0; i<s.length()/2; i++)
+//         {
+//             if(list.contains(s.charAt(i)) )
+//                 count1++;
+//         }
+            
+        
+//         for(int i = s.length()/2; i<s.length(); i++)
+//         {
+//             if(list.contains(s.charAt(i)) )
+//                 count2++;
+//         }
+        
+//         return count1 == count2 ;
+        
+        // we can also use array instead of arraylist
+        
+        String check="aeiouAEIOU";
         
         int count1 =0;
         int count2 =0;
         
         for(int i =0; i<s.length()/2; i++)
         {
-            if(list.contains(s.charAt(i)) )
+            if(check.indexOf(s.charAt(i))  != -1)
                 count1++;
         }
             
         
         for(int i = s.length()/2; i<s.length(); i++)
         {
-            if(list.contains(s.charAt(i)) )
+            if(check.indexOf(s.charAt(i))  != -1)
                 count2++;
         }
         
