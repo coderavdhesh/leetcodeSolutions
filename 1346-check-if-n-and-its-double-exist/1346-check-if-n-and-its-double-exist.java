48224bb19f67179765b63fff095cf301548c6bc5
avdhesh_gupta <61471356+coderavdhesh@users.noreply.github.com>
2022-06-07 08:17:23 +0530

    Time: 4 ms (32.14%), Space: 42.4 MB (70.89%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
@@ -1,58 +1,6 @@
 class Solution {
     public boolean checkIfExist(int[] arr) {
         
-        // this **brute-force** approch having O(n2) time
-        /*for(int i =0; i<arr.length-1; i++)
-        {
-            for(int j =0; j<arr.length; j++)
-            {
-                if(i!=j && arr[i] == 2* arr[j])
-                    return true;
-            }
-        }
-        return false;*/
-        
-        // this code with **Binary Search** is having O(nlogn) compexity of code
-        /*Arrays.sort(arr);
-        
-        for(int i =0; i<arr.length; i++)
-        {
-            int target = 2*arr[i];
-            
-            int left = 0;
-            int right = arr.length-1;
-            
-            while(left<=right)
-            {
-                int mid = left + (right -left) /2;
-                
-                if(arr[mid] == target && mid != i)
-                    return true;
-                else if(target < arr[mid])
-                    right = mid-1;
-                else
-                    left = mid +1;
-                
-            }
-            
-        }
-        return false;*/
-        
-        // this **hashmap** solution is the most optimized with O(n) time
-        
-//         HashMap<Integer, Boolean> hashmap = new HashMap<>();
-        
-//         for(int num : arr) 
-//         {
-//             if(hashmap.containsKey(num * 2) || num % 2 == 0 && hashmap.containsKey(num / 2))
-//                 return true;
-//             else
-//                 hashmap.put(num, true);
-//         }
-//         return false;
-        
-        // the solution with **HashSet** also haing O(n) solution 
-        
         HashSet<Integer> set = new HashSet<>();
         
         for(int i : arr)
@@ -63,5 +11,44 @@ class Solution {
                 set.add(i);
         }
         return false;
+        
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//         HashSet<Integer> set = new HashSet<>();
+        
+//         for(int i : arr)
+//             set.add(i);
+        
+//         for(int i : arr)
+//         {
+//             if(set.contains(2*i) || (i%2 == 0 && set.contains(i/2)))
+//                 return true;
+//         }
+        
+//         return false;
