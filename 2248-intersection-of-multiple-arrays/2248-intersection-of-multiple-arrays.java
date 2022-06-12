5954ca759aa0b1349b44ea0c900494716e7815af
avdhesh_gupta <61471356+coderavdhesh@users.noreply.github.com>
2022-06-12 22:28:51 +0530

    Time: 10 ms (26.81%), Space: 47.4 MB (24.02%) - LeetHub

--- 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.java
+++ 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.java
@@ -1,45 +1,21 @@
 class Solution {
     public List<Integer> intersection(int[][] nums) {
         
-        HashSet<Integer> set = new HashSet<>();
-        
-        for(int num : nums[0])
-            set.add(num);
-        
-        for(int i =1; i<nums.length; i++)
-        {
-            
-            HashSet<Integer> ans_set = new HashSet<>();
-            
-            for(int j =0; j<nums[i].length; j++)
-            {
-                if( set.contains(nums[i][j]) )
-                    ans_set.add(nums[i][j]);
+        HashMap<Integer, Integer> map = new HashMap<>();
+           List<Integer> list = new ArrayList<>();
+           for(int i =0; i < nums.length; i++){
+                for(int j = 0; j < nums[i].length; j++){
+                        map.put(nums[i][j],map.getOrDefault(nums[i][j], 0 ) +1);
+                }
             }
-            set = ans_set;  
-            
-        }
-        
-        ArrayList<Integer> list = new ArrayList<>();
-        
-        for(int i : set)
-            list.add(i);
-        
-        int[] arr = new int[list.size()];
-        
-        for(int i = 0; i< list.size(); i++)
-        {
-            arr[i] = list.get(i);
-        }
-        
-        Arrays.sort(arr);
-        
-        ArrayList<Integer> ans_list = new ArrayList<>();
-        
-        for(int i : arr)
-            ans_list.add(i);
-        
-        return ans_list;
+
+          for(Map.Entry<Integer,Integer> entry : map.entrySet()){
+              if(entry.getValue() == nums.length){
+                  list.add(entry.getKey());
+              }
+          }
+          Collections.sort(list);
+          return list;
         
     }
 }
