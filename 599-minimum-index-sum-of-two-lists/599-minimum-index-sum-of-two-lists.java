94ac7b3ef38794287d5f4d200f05bfa85e4c3ccd
avdhesh_gupta <61471356+coderavdhesh@users.noreply.github.com>
2022-06-17 18:08:39 +0530

    Time: 22 ms (27.37%), Space: 54.7 MB (26.19%) - LeetHub

--- 599-minimum-index-sum-of-two-lists/599-minimum-index-sum-of-two-lists.java
+++ 599-minimum-index-sum-of-two-lists/599-minimum-index-sum-of-two-lists.java
@@ -5,33 +5,31 @@ class Solution {
         HashMap<String, Integer> map1 = new HashMap<>();
         HashMap<String, Integer> map2 = new HashMap<>();
         HashMap<String, Integer> ansMap = new HashMap<>();
-        ArrayList<String> ans = new ArrayList<>();
-        for(int i=0; i<list1.length; i++) {
-            map1.put(list1[i], i);
-        }
         
-         for(int i=0; i<list2.length; i++) {
+        ArrayList<String> ans = new ArrayList<>();
+        
+        for(int i=0; i<list1.length; i++) 
+            map1.put(list1[i], i);
+
+        
+         for(int i=0; i<list2.length; i++) 
             map2.put(list2[i], i);
-        }
+        
         int mIndex = Integer.MAX_VALUE;
-        for(String a: map1.keySet()) {
-            if(map2.containsKey(a) == true) {
+        for(String a: map1.keySet()) 
+            if(map2.containsKey(a) == true) 
                 if((map1.get(a) + map2.get(a)) <= mIndex) {
                     mIndex = map1.get(a) + map2.get(a);
-                    ansMap.put(a, mIndex);
-                }
-            }
-        }
+                    ansMap.put(a, mIndex);}
         
-        for(String a: ansMap.keySet()) {
-            if(ansMap.get(a) == mIndex) {
+        for(String a: ansMap.keySet()) 
+            if(ansMap.get(a) == mIndex) 
                 ans.add(a);
-            }
-        }
+        
         String[] ansA = new String[ans.size()];
-        for(int i=0; i<ansA.length; i++) {
+        for(int i=0; i<ansA.length; i++) 
             ansA[i] = ans.get(i);
-        }
+        
         return ansA;
         
     }
