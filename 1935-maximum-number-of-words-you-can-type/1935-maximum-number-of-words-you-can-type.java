0ea0741a0181355d134951f4cb702b9d744b365d
avdhesh_gupta <61471356+coderavdhesh@users.noreply.github.com>
2022-06-17 13:25:49 +0530

    Time: 8 ms (23.12%), Space: 42.7 MB (61.08%) - LeetHub

--- 1935-maximum-number-of-words-you-can-type/1935-maximum-number-of-words-you-can-type.java
+++ 1935-maximum-number-of-words-you-can-type/1935-maximum-number-of-words-you-can-type.java
@@ -1,22 +1,54 @@
 class Solution {
     public int canBeTypedWords(String text, String brokenLetters) {
         
+        // this code i shaving O(n^2) time
+//         String[] words = text.split("\\s+");
+//         int count = words.length;
         
-        String[] words = text.split("\\s+");
-        int count = words.length;
+//         for(int i=0; i<words.length; i++)
+//         {
+//             String str = words[i];
+//             for(int j =0; j<brokenLetters.length(); j++)
+//            {
+//                 if(str.indexOf(brokenLetters.charAt(j)) != -1)
+//                 {
+//                     count--;
+//                     break;
+//                 }
+//             }
+//         }
+//         return count;
         
-        for(int i=0; i<words.length; i++)
+        // now we use Hashset
+        
+        HashSet<Character> set = new HashSet<>();
+        
+        for(int i =0; i< brokenLetters.length(); i++)
         {
-            String str = words[i];
-            for(int j =0; j<brokenLetters.length(); j++)
-           {
-                if(str.indexOf(brokenLetters.charAt(j)) != -1)
-                {
-                    count--;
-                    break;
-                }
+            set.add( brokenLetters.charAt(i)) ; 
+        }
+        
+        boolean flag = false;
+        int count =0 ;
+        
+        for(int i = 0; i< text.length() ; i++)
+        {
+            if(set.contains(text.charAt(i)) )
+                flag = true;
+        
+            if(text.charAt(i) == ' ')
+            {
+                if(!flag)
+                    count++;
+                
+                flag = false;
             }
         }
+        
+        if(!flag)// if last word is typable with key-board
+            count++;
+        
         return count;
+        
     }
 }
