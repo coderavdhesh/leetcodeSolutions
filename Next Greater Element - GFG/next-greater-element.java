3f1f0f1778c5deda28d24e0f4ba96af3e146de4a
avdhesh_gupta <61471356+coderavdhesh@users.noreply.github.com>
2022-06-30 11:13:44 +0530

    Added solution - LeetHub

---
+++ Next Greater Element - GFG/next-greater-element.java
@@ -0,0 +1,65 @@
+// { Driver Code Starts
+/*package whatever //do not write package name here */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+    
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim());
+		while(t-->0){
+		    int n = Integer.parseInt(br.readLine().trim());
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    long[] arr = new long[n];
+		    for(int i=0; i<n; i++)arr[i]=Long.parseLong(inputLine[i]);
+		    long[] res = new Solution().nextLargerElement(arr, n);
+		    for (int i = 0; i < n; i++) 
+		        System.out.print(res[i] + " ");
+		    System.out.println();
+		}
+	}
+}
+
+
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to find the next greater element for each element of the array.
+    public static long[] nextLargerElement(long[] arr, int n)
+    { 
+        // Your code here
+        
+        Stack<Long> stack = new Stack<>();
+        long[] ary = new long[arr.length];
+        
+        
+        for(int i = arr.length-1; i>=0; i--)
+        {
+            if(stack.isEmpty())
+                ary[i] = -1;
+            else if( !stack.isEmpty() && stack.peek() > arr[i] )
+                ary[i] = stack.peek();
+            else if (!stack.isEmpty() && stack.peek() <= arr[i] )
+            {
+                while(!stack.isEmpty() && stack.peek() <= arr[i] )
+                    stack.pop();
+                
+                if(!stack.isEmpty() )
+                    ary[i] = stack.peek();
+                else
+                    ary[i] = -1;
+            }
+            
+            stack.push(arr[i]);
+        }
+        
+        return ary;
+        
+    } 
+}
