501c2ac05c0a11bc3896ec42ffda74b4eedde4e1
avdhesh_gupta <61471356+coderavdhesh@users.noreply.github.com>
2022-07-02 12:00:01 +0530

    Time: 29 ms (64.24%), Space: 50 MB (88.08%) - LeetHub

--- 962-maximum-width-ramp/962-maximum-width-ramp.java
+++ 962-maximum-width-ramp/962-maximum-width-ramp.java
@@ -18,41 +18,25 @@ class Solution {
         
         // now use stack
         
-//         Stack<Integer> stack = new Stack<>();
+        Stack<Integer> stack = new Stack<>();
         
-//         // lets create a monotonic stack
-//         for(int i =0; i< nums.length; i++)
-//         {
-//             if(stack.isEmpty() || nums[stack.peek()] > nums[i] )
-//                  stack.push(i);
-//         }
-        
-//         int max = 0;
-        
-//         for(int i = nums.length-1; i>=0 ; i--)
-//         {
-//             if(!stack.isEmpty() && nums[i] >= nums[stack.peek()]) 
-//                 max = Math.max(max, i-stack.pop());
-//         }
-        
-//         return max;
-        
-        Stack<Integer> s = new Stack<>();
-        int count=0;
-        // constructing monotonic decreasing Stack of Given Array
-        for(int i=0 ; i< nums.length ; i++){
-            if(s.isEmpty() || nums[s.peek()] > nums[i] ){
-                 s.push(i);
-            }
+        // lets create a monotonic stack
+        for(int i =0; i< nums.length; i++)
+        {
+            if(stack.isEmpty() || nums[stack.peek()] > nums[i] )
+                 stack.push(i);
         }
-        // finding width of ramp
-        int max=0;
-        for(int i=nums.length-1 ; i>=0 ; i--){
-            while(!s.isEmpty() && nums[s.peek()] <= nums[i] ){
-                max = Math.max(i-s.pop(),max);
-            }
+        
+        int max = 0;
+        
+        for(int i = nums.length-1; i>=0 ; i--)
+        {
+            while(!stack.isEmpty() && nums[i] >= nums[stack.peek()]) 
+                max = Math.max(max, i-stack.pop());
         }
+        
         return max;
+        
         
     }
 }
