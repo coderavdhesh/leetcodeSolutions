d7d4f56a2ba1e5d18c06f90464b724638e31d017
avdhesh_gupta <61471356+coderavdhesh@users.noreply.github.com>
2022-07-05 18:10:50 +0530

    Time: 320 ms (7.22%), Space: 42.7 MB (96.39%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
@@ -19,5 +19,20 @@ class Solution {
         
         return count;
         
+//         int l = s.length();
+//         int prefix = 0;
+//         int total = 0;
+//         for (int i = 0; i < l; i++) 
+//         {
+//             if (s.charAt(i) == 'b') 
+//                 prefix++;
+//             else if(s.charAt(i) == 'a' && prefix > 0)
+//             {
+//                 prefix--;
+//                 total++;
+//             }
+//         }
+//         return total;
+        
     }
 }
