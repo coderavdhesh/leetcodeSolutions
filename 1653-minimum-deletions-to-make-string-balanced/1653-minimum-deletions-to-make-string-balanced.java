4b8c10366c440c2f9b5e3f2227234732607ec075
avdhesh_gupta <61471356+coderavdhesh@users.noreply.github.com>
2022-07-05 18:28:33 +0530

    Time: 38 ms (66.62%), Space: 57.8 MB (57.87%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
@@ -1,38 +1,38 @@
 class Solution {
     public int minimumDeletions(String s) {
         
-        Stack<Character> stack = new Stack<>();
-        int count =0 ;
-        for(int i=0; i<s.length(); i++)
-        {
-            char ch = s.charAt(i);
-            
-            if(!stack.isEmpty() && stack.peek() == 'b' && ch == 'a')
-            {
-                stack.pop();
-                count++;
-            }
-            else if (ch == 'b')
-                stack.push(ch);
-        }
-        
-        
-        return count;
-        
-//         int l = s.length();
-//         int prefix = 0;
-//         int total = 0;
-//         for (int i = 0; i < l; i++) 
+//         Stack<Character> stack = new Stack<>();
+//         int count =0 ;
+//         for(int i=0; i<s.length(); i++)
 //         {
-//             if (s.charAt(i) == 'b') 
-//                 prefix++;
-//             else if(s.charAt(i) == 'a' && prefix > 0)
+//             char ch = s.charAt(i);
+            
+//             if(!stack.isEmpty() && stack.peek() == 'b' && ch == 'a')
 //             {
-//                 prefix--;
-//                 total++;
+//                 stack.pop();
+//                 count++;
 //             }
+//             else if (ch == 'b')
+//                 stack.push(ch);
 //         }
-//         return total;
+        
+        
+//         return count;
+        
+        int l = s.length();
+        int prefix = 0;
+        int total = 0;
+        for (int i = 0; i < l; i++) 
+        {
+            if (s.charAt(i) == 'b') 
+                prefix++;
+            else if(s.charAt(i) == 'a' && prefix > 0)
+            {
+                prefix--;
+                total++;
+            }
+        }
+        return total;
         
     }
 }
