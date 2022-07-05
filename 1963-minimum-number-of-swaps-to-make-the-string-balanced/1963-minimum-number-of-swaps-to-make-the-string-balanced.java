b9de5407fd73f16762ccb221e92022ab0027b213
avdhesh_gupta <61471356+coderavdhesh@users.noreply.github.com>
2022-07-05 16:30:59 +0530

    Time: 33 ms (49.13%), Space: 74.1 MB (62.59%) - LeetHub

--- 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.java
+++ 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.java
@@ -1,24 +1,38 @@
 class Solution {
     public int minSwaps(String s) {
         
-        Stack<Character> stack = new Stack<>();
+//         Stack<Character> stack = new Stack<>();
         
-        for(int i =0; i<s.length(); i++)
-        {
-            char ch = s.charAt(i);
+//         for(int i =0; i<s.length(); i++)
+//         {
+//             char ch = s.charAt(i);
             
-            if(ch =='[')
-                stack.push(ch);
-            else
-            {
-                if(!stack.isEmpty() && stack.peek() == '[')
-                    stack.pop();
+//             if(ch =='[')
+//                 stack.push(ch);
+//             else
+//             {
+//                 if(!stack.isEmpty() && stack.peek() == '[')
+//                     stack.pop();
+//                 else
+//                     stack.push(ch);
+//             }
+//         }
+        
+//         return (int)Math.ceil(stack.size()/4.0);
+        
+        
+        
+        int open = 0, close = 0, unbalanced = 0;
+        for(char ch : s.toCharArray()) {
+            if(ch == '[') open++;
+            else {
+                if(open > close) // i.e we have an extra open [ to be used in a pair
+                    close++;
                 else
-                    stack.push(ch);
+                    unbalanced++;
             }
         }
-        
-        return (int)Math.ceil(stack.size()/4.0);
+        return (unbalanced+1)/2;
         
     }
 }
