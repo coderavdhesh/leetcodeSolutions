01c352dc455c183d3b3bb9eaa7bc38b6c34278b9
avdhesh_gupta <61471356+coderavdhesh@users.noreply.github.com>
2022-07-05 16:34:57 +0530

    Time: 360 ms (12.54%), Space: 88.7 MB (14.58%) - LeetHub

--- 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.java
+++ 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.java
@@ -1,39 +1,39 @@
 class Solution {
     public int minSwaps(String s) {
         
-//         Stack<Character> stack = new Stack<>();
+        Stack<Character> stack = new Stack<>();
         
-//         for(int i =0; i<s.length(); i++)
-//         {
-//             char ch = s.charAt(i);
+        for(int i =0; i<s.length(); i++)
+        {
+            char ch = s.charAt(i);
             
-//             if(ch =='[')
-//                 stack.push(ch);
-//             else
-//             {
-//                 if(!stack.isEmpty() && stack.peek() == '[')
-//                     stack.pop();
-//                 else
-//                     stack.push(ch);
-//             }
-//         }
+            if(ch =='[')
+                stack.push(ch);
+            else
+            {
+                if(!stack.isEmpty() && stack.peek() == '[')
+                    stack.pop();
+                else
+                    stack.push(ch);
+            }
+        }
         
-//         return (int)Math.ceil(stack.size()/4.0);
+        return (int)Math.ceil(stack.size()/4.0);
         
         
         
         // coppied code hain ye wala
-        int open = 0, close = 0, unbalanced = 0;
-        for(char ch : s.toCharArray()) {
-            if(ch == '[') open++;
-            else {
-                if(open > close) // i.e we have an extra open [ to be used in a pair
-                    close++;
-                else
-                    unbalanced++;
-            }
-        }
-        return (unbalanced+1)/2;
+        // int open = 0, close = 0, unbalanced = 0;
+        // for(char ch : s.toCharArray()) {
+        //     if(ch == '[') open++;
+        //     else {
+        //         if(open > close) // i.e we have an extra open [ to be used in a pair
+        //             close++;
+        //         else
+        //             unbalanced++;
+        //     }
+        // }
+        // return (unbalanced+1)/2;
         
     }
 }
