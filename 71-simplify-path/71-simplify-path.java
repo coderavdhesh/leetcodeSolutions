5567b252ac0386cf6be437cf296d254fac603a3b
avdhesh_gupta <61471356+coderavdhesh@users.noreply.github.com>
2022-07-09 19:44:53 +0530

    Time: 97 ms (5.64%), Space: 54.7 MB (5.12%) - LeetHub

--- 71-simplify-path/71-simplify-path.java
+++ 71-simplify-path/71-simplify-path.java
@@ -2,14 +2,17 @@ class Solution {
     public String simplifyPath(String path) {
         
         String[] paths = path.split("/");
-        
+        System.out.println(Arrays.toString(paths) );
         Stack<String> stack = new Stack<>();
+        
         for(String curPath : paths)
         {
             if(!stack.empty() && curPath.equals("..")) 
                 stack.pop();
             else if(!curPath.isEmpty() && !curPath.equals(".") && !curPath.equals(".."))
                 stack.push(curPath);
+            
+            System.out.println(stack);
         }
         
         StringBuilder res = new StringBuilder();
