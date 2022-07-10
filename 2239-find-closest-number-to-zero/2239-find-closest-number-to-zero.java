a8bd4ec784e187edf59a344615793df4aae6e118
avdhesh_gupta <61471356+coderavdhesh@users.noreply.github.com>
2022-07-10 18:41:07 +0530

    Time: 14 ms (5.58%), Space: 54.2 MB (5.85%) - LeetHub

--- 2239-find-closest-number-to-zero/2239-find-closest-number-to-zero.java
+++ 2239-find-closest-number-to-zero/2239-find-closest-number-to-zero.java
@@ -1,30 +1,39 @@
 class Solution {
     public int findClosestNumber(int[] nums) {
         
-        int arr[] = new int[2];
+//         int arr[] = new int[2];
         
-        arr[0] = nums[0];// the value
-        arr[1] = Math.abs(nums[0] - 0); // min
+//         arr[0] = nums[0];// the value
+//         arr[1] = Math.abs(nums[0] - 0); // min
         
-        for(int i=1; i<nums.length; i++)
-        {
-            int min = Math.abs(nums[i] - 0);
-            if( min < arr[1])
-            {
-                arr[0] = nums[i];
-                arr[1] = min;
+//         for(int i=1; i<nums.length; i++)
+//         {
+//             int min = Math.abs(nums[i] - 0);
+//             if( min < arr[1])
+//             {
+//                 arr[0] = nums[i];
+//                 arr[1] = min;
+//             }
+//             if( min == arr[1] )
+//             {
+//                 if(arr[0] < nums[i])
+//                 {
+//                     arr[0] = nums[i];
+//                     arr[1] = min;
+//                 }
+//             }
+//         }
+        
+//         return arr[0];
+        
+        PriorityQueue<Integer> heap = new PriorityQueue<>(new Comparator<Integer>(){
+            public int compare(Integer a,Integer b){
+                if(Math.abs(a)==Math.abs(b)) return b-a;
+                return Integer.compare(Math.abs(a),Math.abs(b));
             }
-            if( min == arr[1] )
-            {
-                if(arr[0] < nums[i])
-                {
-                    arr[0] = nums[i];
-                    arr[1] = min;
-                }
-            }
-        }
-        
-        return arr[0];
+        });
+        for(int i:nums) heap.add(i);
+        return heap.remove();
         
     }
 }
