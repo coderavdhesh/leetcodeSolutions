a565d79bf91403fcd1bdd524da62621c67747e1a
avdhesh_gupta <61471356+coderavdhesh@users.noreply.github.com>
2022-07-12 23:34:51 +0530

    Time: 45 ms (30.38%), Space: 67.3 MB (40.08%) - LeetHub

--- 594-longest-harmonious-subsequence/594-longest-harmonious-subsequence.java
+++ 594-longest-harmonious-subsequence/594-longest-harmonious-subsequence.java
@@ -6,25 +6,36 @@ class Solution {
         for(int num : nums)
             map.put(num, map.getOrDefault(num, 0)+1);
         
-        // Min Heap
-        PriorityQueue<Map.Entry<Integer, Integer>> heap =
-            new PriorityQueue<>((a,b) -> a.getKey() - b.getKey() );
+//         // Min Heap
+//         PriorityQueue<Map.Entry<Integer, Integer>> heap =
+//             new PriorityQueue<>((a,b) -> a.getKey() - b.getKey() );
         
-        for(Map.Entry entry : map.entrySet())
-            heap.offer(entry);
+//         for(Map.Entry entry : map.entrySet())
+//             heap.offer(entry);
         
-        int max = 0;
+//         int max = 0;
         
-        while( !heap.isEmpty() )
-        {
-            int top = heap.peek().getKey();
-            int wt = heap.poll().getValue();
+//         while( !heap.isEmpty() )
+//         {
+//             int top = heap.peek().getKey();
+//             int wt = heap.poll().getValue();
             
-            if( !heap.isEmpty() && (heap.peek().getKey() - top) == 1)
-                max = Math.max(max, heap.peek().getValue() + wt);
+//             if( !heap.isEmpty() && (heap.peek().getKey() - top) == 1)
+//                 max = Math.max(max, heap.peek().getValue() + wt);
+//         }
+        
+//         return max;
+        
+        int max_len = 0;
+        for(int keys : map.keySet())
+        {
+            if(map.containsKey(keys+1))
+            {
+                int len  = map.get(keys) + map.get(keys+1);
+                max_len = Math.max(len,max_len);
+            }
         }
         
-        return max;
-        
+        return max_len;
     }
 }
