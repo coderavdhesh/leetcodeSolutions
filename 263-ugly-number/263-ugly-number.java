7c6b9192e7ce34cd7284e71b41eda56730ef4161
avdhesh_gupta <61471356+coderavdhesh@users.noreply.github.com>
2022-07-13 20:25:26 +0530

    Time: 2 ms (66.97%), Space: 42.5 MB (6.29%) - LeetHub

--- 263-ugly-number/263-ugly-number.java
+++ 263-ugly-number/263-ugly-number.java
@@ -1,17 +1,23 @@
 class Solution {
     public boolean isUgly(int n) {
         
-        if(n < 1)
+        if(n == 1)
+            return true;
+        if(n == 0)
             return false;
-            
-        while(n%2== 0)
+                    
+        if(n%2== 0)
             n/=2;
-        while(n%3==0 )
+        else if(n%3==0 )
             n/=3;
-        while( n%5==0)
+        else if( n%5==0)
             n/=5;
+        else
+            return false;
         
-        return n==1 ? true : false;
+        return isUgly(n);
+        
+        
         
     }
 }
