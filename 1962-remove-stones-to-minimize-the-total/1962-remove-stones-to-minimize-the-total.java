510ec92dc18e25942d4fa6494f552ed0785459bd
avdhesh_gupta <61471356+coderavdhesh@users.noreply.github.com>
2022-07-16 00:00:23 +0530

    Time: 982 ms (38.04%), Space: 125.3 MB (9.00%) - LeetHub

--- 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.java
+++ 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.java
@@ -2,26 +2,25 @@ class Solution {
     public int minStoneSum(int[] piles, int k) {
         
         PriorityQueue<Integer> minheap = new PriorityQueue<>(Collections.reverseOrder());
-        int minStones = 0;
-        int totalStones =0;
         
         for(int n : piles)
         {
             minheap.offer(n);
-            totalStones += n;
         }
         
         while( k> 0)
         {
             int num = minheap.poll();
             int removal = (int)Math.floor(num/2);
-            minStones += removal;
             minheap.offer(num - removal);
             k--;
         }
         
-        System.out.println(minheap);
+        int minStones = 0;
+
+        while(!minheap.isEmpty())
+            minStones += minheap.poll();
         
-        return totalStones - minStones;
+        return  minStones;
     }
 }
