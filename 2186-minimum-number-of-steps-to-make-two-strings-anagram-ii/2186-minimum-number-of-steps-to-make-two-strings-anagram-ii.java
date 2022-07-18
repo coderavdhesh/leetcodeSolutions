5f665a3ce5442f304b360694d899913742d1acc9
avdhesh_gupta <61471356+coderavdhesh@users.noreply.github.com>
2022-07-18 14:10:31 +0530

    Time: 176 ms (20.48%), Space: 117.4 MB (8.67%) - LeetHub

--- 2186-minimum-number-of-steps-to-make-two-strings-anagram-ii/2186-minimum-number-of-steps-to-make-two-strings-anagram-ii.java
+++ 2186-minimum-number-of-steps-to-make-two-strings-anagram-ii/2186-minimum-number-of-steps-to-make-two-strings-anagram-ii.java
@@ -1,37 +1,19 @@
 class Solution {
     public int minSteps(String s, String t) {
         
-        HashMap<Character, Integer> map1 = new HashMap<>();
-        HashMap<Character, Integer> map2 = new HashMap<>();
+        HashMap<Character,Integer> hmap = new HashMap<>();
         
-        for(int i =0; i< s.length(); i++)
-        {
-            char ch = s.charAt(i);
-            map1.put(ch, map1.get(ch) == null ? 1 : map1.get(ch)+1 );
-        }
+        for(char ch:s.toCharArray()) 
+            hmap.put(ch,hmap.getOrDefault(ch,0)+1);
         
-        for(int i =0; i< t.length(); i++)
-        {
-            char ch = t.charAt(i);
-            map2.put(ch, map2.get(ch) == null ? 1 : map2.get(ch)+1 );
-        }
+        for(char ch:t.toCharArray())
+            hmap.put(ch,hmap.getOrDefault(ch,0)-1);
         
-        int steps = 0;
+        int count=0;
+        for(char key:hmap.keySet())
+            if(hmap.get(key)!=0)
+                count+=(Math.abs(hmap.get(key)));
+        return count;
         
-        for(char c : map2.keySet())
-        {
-            if( !map1.containsKey(c) )
-                steps += map2.get(c);
-            else
-                steps = steps + Math.abs(map1.get(c) - map2.get(c));
-        }
-        
-        for(char c : map1.keySet())
-        {
-            if( !map2.containsKey(c) )
-                steps += map1.get(c);
-        }
-        
-        return steps;
     }
 }
