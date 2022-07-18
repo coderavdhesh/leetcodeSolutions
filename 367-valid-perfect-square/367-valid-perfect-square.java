ea09e4d5dbc2324a3f0307ae020d3282a79aa088
avdhesh_gupta <61471356+coderavdhesh@users.noreply.github.com>
2022-07-18 20:41:15 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (12.21%) - LeetHub

--- 367-valid-perfect-square/367-valid-perfect-square.java
+++ 367-valid-perfect-square/367-valid-perfect-square.java
@@ -4,20 +4,18 @@ class Solution {
         long start = 0;
         long end = num;
         
-        while(start<=end)
+        while(start <= end)
         {
-            long mid = start + (end -start)/2;
+            long mid = start + (end - start)/2;
             
-            if( mid*mid == num)
+            if(mid*mid == num)
                 return true;
-            else if (mid*mid < num)
-                start = mid + 1;
+            else if ( mid*mid < num )
+                start = mid+1;
             else
                 end = mid-1;
         }
         
         return false;
-        
-        
     }
 }
