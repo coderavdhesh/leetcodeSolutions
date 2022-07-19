246248ba42f7ac81a9716dc157613d587863e92b
avdhesh_gupta <61471356+coderavdhesh@users.noreply.github.com>
2022-07-19 14:36:34 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (89.26%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
@@ -1,27 +1,42 @@
 class Solution {
     public int findKthPositive(int[] arr, int k) {
         
-        ArrayList<Integer> miss = new ArrayList<>();
+//         ArrayList<Integer> miss = new ArrayList<>();
         
-        int i=0;
-        int num = 1;
-        while(i< arr.length)
-        {
-            if(arr[i] == num )
-                i++;
-            else
-                miss.add(num);
+//         int i=0;
+//         int num = 1;
+//         while(i< arr.length)
+//         {
+//             if(arr[i] == num )
+//                 i++;
+//             else
+//                 miss.add(num);
             
-            num++;
+//             num++;
+//         }
+        
+//         int len = miss.size();
+        
+//         if( len < k)
+//             return arr[arr.length-1] + (k -len);
+//         else if(len == 0)
+//             return arr[arr.length-1] + (k);
+        
+//         return miss.get(k-1);
+        
+        int s =0;
+        int e = arr.length;
+        
+        while(s < e)
+        {
+            int mid = s + (e-s)/2;
+            
+            if(arr[mid] - (mid+1) < k)//logic yaha hain
+                s = mid+1;
+            else
+                e = mid;
         }
         
-        int len = miss.size();
-        
-        if( len < k)
-            return arr[arr.length-1] + (k -len);
-        else if(len == 0)
-            return arr[arr.length-1] + (k);
-        
-        return miss.get(k-1);
+        return s+k;
     }
 }
