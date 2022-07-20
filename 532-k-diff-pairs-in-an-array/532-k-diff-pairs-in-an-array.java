db2705d6f47e4f2b85f734cf9648bf5cd0cfa9a0
avdhesh_gupta <61471356+coderavdhesh@users.noreply.github.com>
2022-07-20 20:37:21 +0530

    Time: 17 ms (40.65%), Space: 48.8 MB (22.29%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.java
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.java
@@ -1,34 +1,49 @@
 class Solution {
     public int findPairs(int[] nums, int k) {
         
-        Arrays.sort(nums);
+//         Arrays.sort(nums);
+//         int count =0;
+        
+//         for(int i =0; i<nums.length-1; i++)
+//         {
+//             if(i == 0 || (nums[i] != nums[i-1] && i!=0) )
+//             {
+//                 if(binarySearch(nums, i+1, nums.length-1, nums[i]+k))
+//                     count++;
+//             }  
+//         }
+//         return count;
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for(int n : nums)
+            map.put(n, map.get(n) == null? 1 : map.get(n)+1);
+        
         int count =0;
         
-        for(int i =0; i<nums.length-1; i++)
+        for(int key : map.keySet())
         {
-            if(i == 0 || (nums[i] != nums[i-1] && i!=0) )
-            {
-                if(binarySearch(nums, i+1, nums.length-1, nums[i]+k))
-                    count++;
-            }  
+            if((k>0 && map.containsKey(key+k) )|| (k == 0 && map.get(key)>1) )
+                count++;
         }
+        
         return count;
         
     }
     
-    public boolean binarySearch(int[] arr, int s, int e, int target){
+//     public boolean binarySearch(int[] arr, int s, int e, int target){
         
-        while(s<=e)
-        {
-            int mid = s +(e-s)/2;
+//         while(s<=e)
+//         {
+//             int mid = s +(e-s)/2;
             
-            if(arr[mid] == target)
-                return true;
-            else if(arr[mid] < target)
-                s = mid+1;
-            else 
-                e = mid-1;
-        }
-        return false;
-    }
+//             if(arr[mid] == target)
+//                 return true;
+//             else if(arr[mid] < target)
+//                 s = mid+1;
+//             else 
+//                 e = mid-1;
+//         }
+//         return false;
+//     }
 }
