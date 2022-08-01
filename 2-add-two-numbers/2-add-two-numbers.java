5b8a7dee86c1c30f3b5d1fb5b49eeca653611455
avdhesh_gupta <61471356+coderavdhesh@users.noreply.github.com>
2022-08-01 18:09:27 +0530

    Time: 3 ms (80.53%), Space: 48 MB (40.34%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.java
+++ 2-add-two-numbers/2-add-two-numbers.java
@@ -11,79 +11,39 @@
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         
-        
-        ListNode cur1 = l1;
-        ListNode cur2 = l2;
-        
+        ListNode ansNode = new ListNode(0);
+        ListNode dummy = ansNode;
         int carry = 0;
         
-        ListNode ansNode = new ListNode(0);
-        ListNode ans = ansNode;
-        
-        while( cur1 != null && cur2 != null )
+        while( l1 != null || l2 != null || carry>0)
         {
-            int num = (cur1.val +cur2.val) + carry;
+            int sum = 0;
+            if(l1 != null)
+            {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if(l2 != null)
+            {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            sum += carry;
             
-            if(num > 9)
+            if(sum > 9)
             {
-                ans.next = new ListNode(num%10);
-                carry = num/10;
-                ans = ans.next;
+                dummy.next = new ListNode(sum%10);
+                carry = sum/10;
+                dummy = dummy.next;
             }
             else
             {
-                ans.next = new ListNode(num);
+                dummy.next = new ListNode(sum);
                 carry = 0;
-                ans = ans.next;
+                dummy = dummy.next;
             }
-            
-            cur1 = cur1.next;
-            cur2 = cur2.next;
         }
-        
-       
-    
-        while(cur1 != null)
-        {
-            int n = cur1.val +carry;
-            if(n>9)
-            {
-                ans.next = new ListNode(n%10);
-                ans =ans.next;
-                carry = n/10;
-            }
-            else
-            {
-                ans.next = new ListNode(n);
-                carry = 0;
-                ans = ans.next;
-            }
-            cur1 = cur1.next;
-        }
-        
-        while(cur2 != null)
-        {
-            int n = cur2.val +carry;
-            if(n>9)
-            {
-                ans.next = new ListNode(n%10);
-                ans =ans.next;
-                carry = n/10;
-            }
-            else
-            {
-                ans.next = new ListNode(n);
-                carry = 0;
-                ans = ans.next;
-            }
-            cur2 = cur2.next;
-        }
-        
-        if(carry != 0)
-           ans.next = new ListNode(carry);
-        
         ansNode = ansNode.next;
         return ansNode;
-        
     }
 }
