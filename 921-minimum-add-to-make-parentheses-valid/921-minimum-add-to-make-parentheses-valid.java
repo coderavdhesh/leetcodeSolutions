4612eefa6da4e59a9e7dccd8144c73b3492892e0
avdhesh_gupta <61471356+coderavdhesh@users.noreply.github.com>
2022-08-23 12:51:44 +0530

    Time: 2 ms (47.53%), Space: 42 MB (61.45%) - LeetHub

--- 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.java
+++ 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.java
@@ -1,61 +1,18 @@
 class Solution {
     public int minAddToMakeValid(String s) {
         
-        // Method One : stack wala
-        /*
         Stack<Character> stack = new Stack<>();
-        int moves =0;
+        int count=0;
         
-        for(int i =0; i< s.length() ; i++)
-        {
-            char ch = s.charAt(i);
-            
-            if(ch == '(')
-                stack.push(ch);
-            else
-            {
-                if(!stack.isEmpty())
-                    stack.pop();
-                else
-                    moves++;
-            }
+        for(int i =0; i<s.length(); i++){
+            if(s.charAt(i) == '(')
+                stack.push('(');
+            else if(!stack.isEmpty() && s.charAt(i) == ')')
+                stack.pop();
+            else if(stack.isEmpty() && s.charAt(i) == ')')
+                count++;
         }
         
-        while(!stack.isEmpty())
-        {
-            moves++;
-            stack.pop();
-        }
-        
-        return moves;     */
-        
-        
-        int open =0 ;
-        int close = 0;
-        int needs = 0;
-        
-        for(int i =0; i< s.length() ; i++)
-        {
-            char ch = s.charAt(i);
-            if(ch == '(')
-                open++;
-            else
-            {
-                if(open>close)
-                {
-                    close++;
-                    // open--;
-                }
-                else
-                    needs++;
-            }
-        }
-        
-        int n = (open-close);
-        
-        if(n>0)
-            needs+=n;
-        
-        return needs;
+        return stack.size()+count;
     }
 }
