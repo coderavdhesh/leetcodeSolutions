1f116240a6cab35a6dec570ddf2960e55f9e4335
avdhesh_gupta <61471356+coderavdhesh@users.noreply.github.com>
2022-09-06 10:14:27 +0530

    Time: 4 ms (39.25%), Space: 42.7 MB (23.08%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.java
+++ 389-find-the-difference/389-find-the-difference.java
@@ -1,38 +1,15 @@
 class Solution {
     public char findTheDifference(String s, String t) {
         
-        // this code is having o(n) time but consuming more space
-        /*HashMap<Character,Integer> mapS = new HashMap<>();
-        HashMap<Character,Integer> mapT = new HashMap<>();
+        char ch = ' ';
         
-        for(int i =0; i<s.length(); i++)
-            mapS.put(s.charAt(i), mapS.get(s.charAt(i)) == null ? 1 : mapS.get(s.charAt(i))+1 );
-        
-        for(int i =0; i<t.length(); i++)
-            mapT.put(t.charAt(i), mapT.get(t.charAt(i)) == null ? 1 : mapT.get(t.charAt(i))+1 );
-        
-        if(mapS.size() == 0)
-            return t.charAt(0);
-        
-        for(char ch : mapT.keySet())
-        {
-            if( mapS.get(ch) != mapT.get(ch) )
-                return ch;
+        for(int i =0; i<s.length(); i++){
+            ch ^= s.charAt(i);
         }
-
-        return ' ';*/
+        for(int i =0; i<t.length(); i++){
+            ch ^= t.charAt(i);
+        }
         
-        // Let's try the bit manipulation and much better solution
-        
-        char xor = ' ';
-        
-        for(int i =0; i<s.length() ; i++)
-            xor ^= s.charAt(i);
-        
-        for(int i =0; i<t.length() ; i++)
-            xor ^= t.charAt(i);
-        
-        return Character.toLowerCase(xor);
-        
+        return Character.toLowerCase(ch);
     }
 }
