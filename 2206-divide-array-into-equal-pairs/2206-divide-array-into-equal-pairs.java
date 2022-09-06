612ee4e048d10ed0d0af8a797c0a5aac6638ed96
avdhesh_gupta <61471356+coderavdhesh@users.noreply.github.com>
2022-09-06 20:26:27 +0530

    Time: 42 ms (5.08%), Space: 50.1 MB (5.50%) - LeetHub

--- 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.java
+++ 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.java
@@ -3,14 +3,17 @@ class Solution {
         
         HashMap<Integer, Integer> map = new HashMap<>();
         
-        for(int n : nums)
-            map.put(n, map.get(n) == null ? 1 : map.get(n)+1 );
+        for(int n : nums){
+            map.put(n, map.get(n) == null ? 1 : map.get(n)+1);
+        }
         
-        for(int key : map.keySet() )
+        System.out.println(map);
+        
+        for(int key : map.keySet()){
             if(map.get(key)%2 != 0)
                 return false;
+        }
         
         return true;
-        
     }
 }
